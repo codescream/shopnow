56c9173919323c3ed454ce647cfe15da670e0769
ecclesia <ogilom@gmail.com>
2023-12-19 13:36:22 -0500

    correct strip authorization issues

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -5,8 +5,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    console.log(req.body);
-
     const params = { 
       submit_type: 'pay',
       payment_method_types: ['card'],
